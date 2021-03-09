d58c61a0dfbed07fd4f6a9bc5050f13c1ae8022e
EvertonAvelino <everton-vitor@hotmail.com>
2021-03-09 18:49:26 -0300

    alterando parceiros

--- index.php
+++ index.php
@@ -24,55 +24,55 @@
     <header>
 
         <nav class="menu fixed-top">
-            
+
 
             <img class="logo" src="img/logo-qualitec.png" alt="logo">
             <ul class="nav__links">
-                <li ><a href="#home">Empresa</a></li>
-                <li ><a href="#servicos">Serviços</a></li>
-                <li ><a href="#parceiros">Parceiros</a></li>
-                <li ><a href="#representantes">Representantes</a></li>
-                <li ><a href="#localizacao">localização</a></li>
+                <li><a href="#home">Empresa</a></li>
+                <li><a href="#servicos">Serviços</a></li>
+                <li><a href="#parceiros">Parceiros</a></li>
+                <li><a href="#representantes">Representantes</a></li>
+                <li><a href="#localizacao">localização</a></li>
             </ul>
             <a class="nav-link" href="contato.php"><button>Contato</button></a>
-            
+
         </nav>
         <!--fim do menu -->
 
         <!--inicio slide -->
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-inner" role="listbox">
-                        <?php
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-inner" role="listbox">
+                    <?php
 						$controle_ativo = 2;						
 						$result_carousel = "SELECT * FROM carrouses ORDER BY id ASC";
 						$resultado_carousel = mysqli_query($conn, $result_carousel);
 						while($row_carousel = mysqli_fetch_assoc($resultado_carousel)){ 
 							if($controle_ativo == 2){ ?>
-                        <div class="carousel-item active">
-                            <img src="img/carousel/<?php echo $row_carousel['imagen_carousel']; ?>"
-                                class="d-block w-100" alt="<?php echo $row_carousel['nome']; ?>">
-                        </div><?php
+                    <div class="carousel-item active">
+                        <img src="img/carousel/<?php echo $row_carousel['imagen_carousel']; ?>" class="d-block w-100"
+                            alt="<?php echo $row_carousel['nome']; ?>">
+                    </div><?php
                         	$controle_ativo = 1;
 							}else{ ?>
-                        <div class="carousel-item">
-                            <img src="img/carousel/<?php echo $row_carousel['imagen_carousel']; ?>"
-                                class="d-block w-100" alt="<?php echo $row_carousel['nome']; ?>">
-                        </div> <?php
+                    <div class="carousel-item">
+                        <img src="img/carousel/<?php echo $row_carousel['imagen_carousel']; ?>" class="d-block w-100"
+                            alt="<?php echo $row_carousel['nome']; ?>">
+                    </div> <?php
 							}
 						}
 					?>
-                    </div>
-                    <!-- Controles -->
-                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
                 </div>
+                <!-- Controles -->
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
             <!--fim  slide -->
     </header>
 
@@ -87,6 +87,18 @@
 
             </div>
             <div id="parceiros" class="container-fluid">
+                <img id="partners" src="#">
+                <img id="partners" src="#">
+                <img id="partners" src="#">
+                <img id="partners" src="#">
+                <img id="partners" src="#">
+                <img id="partners" src="#">
+                <img id="partners" src="#">
+                <img id="partners" src="#">
+                <img id="partners" src="#">
+                <img id="partners" src="#">
+                <img id="partners" src="#">
+                <img id="partners" src="#">
             </div>
             <div id="representantes" class="container-fluid">
                 <h1>Representantes</h1>
@@ -100,9 +112,9 @@
         <a class="whatsapp-link" href="https://api.whatsapp.com/send?phone=5532984050969&text=Olá!" target="_blank">
             <i class="fa fa-whatsapp"></i>
         </a>
-    
-</main>
-        
+
+    </main>
+
 
     <footer>
 
