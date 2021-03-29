b858ff35614b57918c65f10f85cf7dcbb0b392ec
EvertonAvelino <everton-vitor@hotmail.com>
2021-03-29 00:28:06 -0300

    implementando form de contato com phpMailer, obs: resolver problema com caracteres especiais

--- contato.php
+++ contato.php
@@ -1,3 +1,13 @@
+<?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    include_once './conexao.php';
+    require './lib/vendor/autoload.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -34,7 +44,7 @@
                 <li><a href="index.html #servicos">Serviços</a></li>
                 <li><a href="index.html #parceiros">Parceiros</a></li>
                 <li><a href="index.html #representantes">Representantes</a></li>
-                <li><a href="#localizacao">localização</a></li>
+                <li><a href="index.html #localizacao">localização</a></li>
 
             </ul>
             <a class="nav-link" href="contato.php"><button>Contato</button></a>
@@ -91,10 +101,76 @@
             <a class="carousel-control-next" href="#demo" data-slide="next">
                 <span class="carousel-control-next-icon"></span>
             </a>
-            -->
-        </div>
 
+        </div>
         <!--fim  slide -->
+        <main>
+            <h1>Formulario de contato</h1>
+            <form name="add_msg" action="" method="POST">
+                <label>Nome:</label>
+                <input type="TEXT" name="name" id="name" placeholder="Nome completo" required><br>
+                <label>Email:</label>
+                <input type="email" name="email" id="email" placeholder="Seu email" required><br>
+                <label>Assunto:</label>
+                <input type="TEXT" name="assunto" id="assunto" placeholder="assunto" required><br>
+                <label>Mensagem:</label>
+                <input type="TEXT" name="mensagem" id="mensagem" placeholder="mensagem" required><br>
+                <input type="submit" value="Enviar" name="sendAddMsg">
+            </form>
+            <?php               
+                $data = filter_input_array(INPUT_POST,FILTER_DEFAULT);
+                
+                if(!empty($data['sendAddMsg'])){
+                   var_dump($data);
+                  $query_msg = "INSERT INTO contatos_msgs (name,email,assunto,mensagem, data_envio) VALUES (:name, :email, :assunto, :mensagem, NOW())";
+
+                  $add_msg = $conn->prepare($query_msg);
+
+                  $add_msg ->bindParam(':name',$data['name'], PDO::PARAM_STR);
+                  $add_msg ->bindParam(':email',$data['email'], PDO::PARAM_STR);
+                  $add_msg ->bindParam(':assunto',$data['assunto'], PDO::PARAM_STR);
+                  $add_msg ->bindParam(':mensagem',$data['mensagem'], PDO::PARAM_STR);
+
+                  $add_msg ->execute();
+
+                  if($add_msg->rowCount()){
+
+                    $mail = new PHPMailer(true);
+
+                      try {
+                        //Server settings
+                        //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+                        $mail->CharSet = 'UTF-8';
+                        $mail->isSMTP();
+                        $mail->Host = 'smtp.mailtrap.io';
+                        $mail->SMTPAuth = true;
+                        $mail->Username = '814ec1619793f1';
+                        $mail->Password = '8e1c263bd0e2ef';
+                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                        $mail->Port = 2525;
+
+                        //Recipients
+                        $mail->setFrom('evertonvitorfdv@gmail.com', 'Qualitec ');
+                        $mail->addAddress($data['email'], $data['name']);
+
+                        // Content
+                        $mail->isHTML(true);                                  
+                        $mail->Subject = $data['assunto'];
+                        $mail->Body = $data['mensagem'];
+                        $mail->AltBody = $data['assunto'];
+
+                        $mail->send();
+                        echo "<script>alert('Email de conatato enviado con sucesso')</script>";
+                    } catch (Exception $e) {
+                        echo "<script>alert('Erro ao enviar mensagem ')</script>";
+                    }
+                      
+                  }else{
+                      echo "<script>alert('Erro ao enviar mensagem ')</script>";
+                  }
+                }          
+            ?>
+        </main>
     </header>
     <section>
 
