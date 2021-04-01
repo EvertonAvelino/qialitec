f9de9e6b33266ab5edd0149d6830caa8d469bf55
EvertonAvelino <everton-vitor@hotmail.com>
2021-04-01 20:06:00 -0300

    alterando estrutura do projeto

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+    include ("./header.html");
+?>
+
+< class="container">
+    <section>
+        <div id="home" class="container-fluid">
+            <h1>home</h1>
+        </div>
+        <div id="servicos" class="container-fluid">
+            <h1>Serviços</h1>
+
+        </div>
+        <div id="parceiros" class="container-fluid">
+            <img id="partners" src="#">
+            <img id="partners" src="#">
+            <img id="partners" src="#">
+            <img id="partners" src="#">
+            <img id="partners" src="#">
+            <img id="partners" src="#">
+            <img id="partners" src="#">
+            <img id="partners" src="#">
+            <img id="partners" src="#">
+            <img id="partners" src="#">
+            <img id="partners" src="#">
+            <img id="partners" src="#">
+        </div>
+        <div id="representantes" class="container-fluid">
+            <h1>Representantes</h1>
+        </div>
+        <div id="localizacao" class="container-fluid">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d840.7863764532725!2d-42.80304468129711!3d-20.854835363332107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa343e90cfbbfd1%3A0x59c58f2c005e459a!2sQualitec!5e1!3m2!1spt-BR!2sbr!4v1616034996562!5m2!1spt-BR!2sbr"
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+
+    </section>
+    <!-- link whatsapp-->
+    <a class="whatsapp-link" href="https://api.whatsapp.com/send?phone=5532984050969&text=Olá!" target="_blank">
+        <i class="fa fa-whatsapp"></i>
+    </a>
+
+
+
+
+    <?php
+    include ("footer.html");
+?>
