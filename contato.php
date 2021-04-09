59d85f835ee10bd830bcf9d57e8f4187c22c3da4
EvertonAvelino <everton-vitor@hotmail.com>
2021-04-09 13:42:48 -0300

    modificando designer do formulario

--- contato.php
+++ contato.php
@@ -5,21 +5,52 @@
 
 
 <main>
-    <h1>Formulario de contato</h1>
-    <form name="add_msg" action="email.php" method="POST">
-        <label>Nome:</label>
-        <input type="TEXT" name="name" id="name" placeholder="Nome completo" required><br>
-        <label>Telefone:</label>
-        <input type="TEXT" name="telefone" id="name" placeholder="Seu numero de telefone" required><br>
-        <label>Email:</label>
-        <input type="email" name="email" id="email" placeholder="Seu email" required><br>
-        <label>Assunto:</label>
-        <input type="TEXT" name="assunto" id="assunto" placeholder="assunto" required><br>
-        <label>Mensagem:</label>
-        <input type="TEXT" name="mensagem" id="mensagem" placeholder="mensagem" required><br>
-        <input type="submit" value="Enviar" name="sendAddMsg">
-    </form>
+    <div class="jumbotron text-md-start descr-top" id="missao">
+        <div class="container">
+            <h2 class="text-left">Fale Conosco</h2>
+            <hr>
 
+            <form name="add_msg" action="email.php" method="POST">
+                <div class="form-group">
+                    <label>
+                        <h5>Nome:</h5>
+                    </label>
+                    <input type="TEXT" class="form-control" name="name" id="name" placeholder="Nome completo"
+                        required><br>
+                </div>
+                <div class="form-group">
+                    <label>
+                        <h5>Telefone:</h5>
+                    </label>
+                    <input type="TEXT" class="form-control" name="telefone" id="name"
+                        placeholder="Seu numero de telefone" required><br>
+                </div>
+                <div class="form-group">
+                    <label>
+                        <h5>Email:</h5>
+                    </label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Seu email"
+                        required><br>
+                </div>
+                <div class="form-group">
+                    <label>
+                        <h5>Assunto:</h5>
+                    </label>
+                    <input type="TEXT" class="form-control" name="assunto" id="assunto" placeholder="assunto"
+                        required><br>
+                </div>
+                <div class="form-group">
+                    <label>
+                        <h5>Mensagem:</h5>
+                    </label>
+                    <textarea class="form-control" name="mensagem" id="mensagem" placeholder="mensagem" cols="45"
+                        rows="8" maxlength="6555" required></textarea>
+                    <button class="btn btn-success" type="submit" name="sendAddMsg">Enviar</button>
+                </div>
+        </div>
+        </form>
+    </div>
+    </div>
 
 </main>
 </header>
